dd7f953a857e63da01510b5bc82cbdaf1790dc4d
Aakash Tyagi <aakashtyagi1996@outlook.com>
2020-09-04 12:05:04 +0530

    Component Customize (#5)
    Components

--- skyux-spa-components/src/app/app-extras.module.ts
+++ skyux-spa-components/src/app/app-extras.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { AppSkyModule } from './app-sky.module';
+// import { SkyVerticalTabDemoModule } from './components/vertical-tabs-demo.module';
 
 @NgModule({
   exports: [
     AppSkyModule
+    // SkyVerticalTabDemoModule
   ]
 })
 export class AppExtrasModule { }
